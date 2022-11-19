80d7b29f29af6befadf13d84e0b395b152315883
Rdotsun <ruiiwa30@gmail.com>
2022-11-19 22:57:38 +0900

    setting vpc_security_group_ids

--- example/main.tf
+++ example/main.tf
@@ -1,6 +1,29 @@
+resource "aws_security_group" "example_ec2" {
+  name = "example-ec2"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "example_instance_type" {
+  default = "t3.micro"
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0c3fd0f5d33134a76"
-  instance_type = "t3.micro"
+  instance_type = var.example_instance_type
+  vpc_security_group_ids = [aws_security_group.example_ec2.id]
 
   user_data = <<EOF
     #!/bin/bash
@@ -11,4 +34,8 @@ EOF
   tags = {
     Name = "example"
   }
+}
+
+output "example_public_dns" {
+  value = aws_instance.example.public_dns
 }
