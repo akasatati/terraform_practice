fc8be64ac2395fb1b16eb0ca53d0917931c1fec0
Rdotsun <ruiiwa30@gmail.com>
2022-11-21 00:15:40 +0900

    fix

--- vpc.tf
+++ vpc.tf
@@ -92,13 +92,13 @@ resource "aws_route_table" "private_1" {
 
 resource "aws_route" "private_0" {
   route_table_id         = aws_route_table.private_0.id
-  nat_gateway_id         = aws_nat_gateway_0.example.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_0.id
   destination_cidr_block = "0.0.0.0/0"
 }
 
 resource "aws_route" "private_1" {
   route_table_id         = aws_route_table.private_1.id
-  nat_gateway_id         = aws_nat_gateway_1.example.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_1.id
   destination_cidr_block = "0.0.0.0/0"
 }
 
