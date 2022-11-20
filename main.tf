227b0ebfdd48eff307c1dcef5d67004b28544aaa
Rdotsun <ruiiwa30@gmail.com>
2022-11-20 14:01:13 +0900

    fix iam_role

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:DescribeRegions"] # リージョン一覧を取得する
+    resources = ["*"]
+  }
+}
+
 module "describe_regions_for_ec2" {
   source     = "./iam_role"
   name       = "describe-regions-for-ec2"
